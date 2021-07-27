1428803017b2ea17b29ab91f2bf7d8232a3b9d77
hfhbd <hfhbd@users.noreply.github.com>
2021-07-27 19:40:46 +0200

    Use new window api

--- src/main/kotlin/fh/ws17/conterm/gui/View.kt
+++ src/main/kotlin/fh/ws17/conterm/gui/View.kt
@@ -1,24 +1,31 @@
 package fh.ws17.conterm.gui
 
-import androidx.compose.desktop.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.painter.*
+import androidx.compose.ui.input.key.*
 import androidx.compose.ui.res.*
 import androidx.compose.ui.unit.*
+import androidx.compose.ui.window.*
 import fh.ws17.conterm.*
 import fh.ws17.conterm.Stack as ContermStack
 
 @Composable
-private fun ContainerButton(container: Container, onClick: (Container) -> Unit, modifier: Modifier) =
+private fun ContainerButton(
+    container: Container,
+    onClick: (Container) -> Unit,
+    modifier: Modifier,
+    image: Painter = painterResource("Container.jpg")
+) =
     Button(onClick = { onClick(container) }, modifier = modifier) {
         val colorFilter = if (container.isStable) {
             ColorFilter.tint(Color.Blue, BlendMode.Hue)
         } else null
-        Image(imageResource("Container.jpg"), null, colorFilter = colorFilter)
+        Image(image, null, colorFilter = colorFilter)
     }
 
 @Composable
@@ -37,45 +44,61 @@ private fun stockView(stock: ContermStack<ContermStack<Container>>, onContainerC
     }
 }
 
-internal fun View(viewModel: ViewModel) = Window(title = viewModel.title, size = IntSize(400, 400)) {
-    val stock by viewModel.toState()
-    var text: String? by remember { mutableStateOf(null) }
-    Column {
-        Row {
-            stockView(stock) {
-                text = viewModel.onContainerClicked(it)
-            }
-        }
-        Row {
-            Button(onClick = {
-                text = viewModel.onBiggerClicked()
-            }) {
-                Text(">")
-            }
-            Button(onClick = {
-                text = viewModel.onExclamationMarkClicked()
-            }) {
-                Text("!")
-            }
-            Button(onClick = {
-                text = viewModel.onQuestionMarkClicked()
-            }) {
-                Text("?")
-            }
-        }
-    }
-    text?.let { it ->
-        AlertDialog(
-            onDismissRequest = { text = null },
-            title = { Text("Information") },
-            text = { Text(it) },
-            confirmButton = {
-                Button(onClick = {
-                    text = null
-                }) {
-                    Text("OK")
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+internal fun View(viewModel: ViewModel) {
+    var show by remember { mutableStateOf(true) }
+    if (show) {
+        Window(
+            onKeyEvent = {
+                if (it.key == Key.W && it.isMetaPressed && it.type == KeyEventType.KeyUp) {
+                    show = false
+                    false
+                } else true
+            },
+            onCloseRequest = { show = false }, title = viewModel.title, resizable = true,
+            state = WindowState(size = WindowSize(400.dp, 300.dp))
+        ) {
+            val stock by viewModel.toState()
+            var text: String? by remember { mutableStateOf(null) }
+            Column {
+                Row {
+                    stockView(stock) {
+                        text = viewModel.onContainerClicked(it)
+                    }
+                }
+                Row {
+                    Button(onClick = {
+                        text = viewModel.onBiggerClicked()
+                    }) {
+                        Text(">")
+                    }
+                    Button(onClick = {
+                        text = viewModel.onExclamationMarkClicked()
+                    }) {
+                        Text("!")
+                    }
+                    Button(onClick = {
+                        text = viewModel.onQuestionMarkClicked()
+                    }) {
+                        Text("?")
+                    }
                 }
             }
-        )
+            text?.let { it ->
+                Dialog(onCloseRequest = {
+                    text = null
+                }, title = "Information") {
+                    Column {
+                        Text(it)
+                        Button(onClick = {
+                            text = null
+                        }) {
+                            Text("OK")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
