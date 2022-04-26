408ed15b2d19598b672b891ee963127e7d2075da
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-04-26 09:20:00 +0200

    Bump Compose to 1.2.0-alpha01-dev675 and Kotlin to 1.6.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
-    id("org.jetbrains.compose") version "1.1.1"
+    kotlin("jvm") version "1.6.21"
+    id("org.jetbrains.compose") version "1.2.0-alpha01-dev675"
 }
 
 repositories {
