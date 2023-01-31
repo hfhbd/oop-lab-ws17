cf36636a8af2f5abb5cf00f19d4ba4528606023a
hfhbd <hfhbd@users.noreply.github.com>
2023-01-31 08:46:22 +0100

    Bump Kotlin to 1.8 and Compose to 1.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,8 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
 
