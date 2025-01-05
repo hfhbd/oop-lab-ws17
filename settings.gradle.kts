58d74e1d1a435fe7c2f4e4dc843855964a0a519f
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2025-01-05 17:58:58 +0100

    Update project (#194)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,23 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+    id("com.gradle.develocity") version "3.19"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/terms-of-service")
+        termsOfUseAgree.set("yes")
+        val isCI = providers.environmentVariable("CI").isPresent
+        publishing {
+            onlyIf { isCI }
+        }
+        tag("CI")
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -16,4 +33,3 @@ dependencyResolutionManagement {
 rootProject.name = "oop-lab-ws17"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
